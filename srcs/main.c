8d386f91cdd0aa4b801f52cfcab7cc4623c10dd1
1mthe0wl <leetpwnsroot@gmail.com>
2022-01-22 18:05:56 +0100

    Nice mlx

--- srcs/main.c
+++ srcs/main.c
@@ -6,20 +6,43 @@
 /*   By: hsabir <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/22 14:19:03 by hsabir            #+#    #+#             */
-/*   Updated: 2022/01/22 14:53:39 by hsabir           ###   ########.fr       */
+/*   Updated: 2022/01/22 18:03:23 by hsabir           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minirt.h>
 //#include <mlx.h>
 
+typedef struct s_data
+{
+	void	*img;
+	char	*addr;
+	int		bbp;
+	int		line_len;
+	int		endian;
+}	t_data;
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_len + x * (data->bbp / 8));
+	*(unsigned int*)dst = color;
+}
+
 int main(void)
 {
-/*	void	*mlx;
+	void	*mlx;
 	void	*mlx_win;
+	t_data	img;
 
 	mlx = mlx_init();
 	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello MiniRT!");
-	mlx_loop(mlx);*/
-	ft_printf(1, "MiniRT\n");
+	img.img = mlx_new_image(mlx, 1920, 1080);
+	img.addr = mlx_get_data_addr(img.img, &img.bbp, &img.line_len, &img.endian);
+	for (int i = 0; i < 1920; i++)
+		for (int j = 0; j < 1080; j++)
+				my_mlx_pixel_put(&img, i, j, 0x00FF0000 + i * j);
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	mlx_loop(mlx);
 }
