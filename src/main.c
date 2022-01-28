c0163bc0fcdf6a092e4fff07443cb0fb8e22375f
Mano Ségransan <msegrans@student.42lausanne.ch>
2022-01-28 17:40:28 +0100

    Calculate distance

--- src/main.c
+++ src/main.c
@@ -18,29 +18,95 @@
 #include <types.h>
 #include <op_vec_double.h>
 #include <op_vec.h>
+#include <float.h>
 
-int	hit_sphere(const t_point center, double radius, t_ray r)
+#include <math.h>
+
+double	solve_quadratic(double a, double b, double c)
+{
+	double d;
+	double t0;
+	double t1;
+	double q;
+
+	d = b * b - 4 * a * c;
+	if (d < 0)
+		return (-1);
+	if (d == 0)
+		return (- 0.5 * b / a);
+
+	if (b > 0)
+		q = -0.5 * (b + sqrt(d));
+	else
+		q = -0.5 * (b - sqrt(d));
+
+	t0 = q / a;
+	t1 = c / q;
+
+	if (t0 > t1)
+	{
+		q = t1;
+		t1 = t0;
+		t0 = q;
+	}
+	if (t0 < 0)
+		return (t1);
+	return (t0);
+}
+
+double	hit_sphere(const t_sphere *sphere, t_ray r)
 {
 	t_vec3	oc;
+	double	radius;
 	double	a;
 	double	b;
 	double	c;
 
-	oc = min_vec(r.origin, center);
+	radius = sphere->diameter / 2;
+	oc = min_vec(r.origin, sphere->coordinates);
 	a = dot(r.direction, r.direction);
-	b = 2.0 * dot(oc, r.direction);
+	b = 2.0 * dot(r.direction, oc);
 	c = dot(oc, oc) - radius * radius;
-	return (b * b - 4 * a * c > 0);
+	return (solve_quadratic(a, b, c));
 }
 
-int	ray_color(t_ray r)
+int rgb_to_int(t_colour c)
 {
-	t_point sphere = {0, 0, -1};
-	if (hit_sphere(sphere, 0.2, r))
-		return (0xFFFFFF);
-	t_vec3 unit_direction = unit_vector(r.direction);
-	double t = 0.5 * (unit_direction.y + 1.0);
-	return (int)((1.0 - t) * 0xFFFFFF + t * 0xFF0000);
+	int rgb;
+
+	rgb = (int)c.x;
+	rgb = (rgb << 8) + (int)c.y;
+	rgb = (rgb << 8) + (int)c.z;
+	return (rgb);
+}
+
+int	ray_color(t_ray r, t_list **head)
+{
+	double distance = DBL_MAX;
+	double t;
+	t_list *elem;
+	t_list *hit_elem = NULL;
+
+	elem = *head;
+	while (elem != NULL)
+	{
+		if (elem->type == 's')
+		{
+			t = hit_sphere(((t_sphere *)elem->content), r);
+			if (t >= 0 && t < distance)
+			{
+				distance = t;
+				hit_elem = elem;
+			}
+		}
+		elem = elem->next;
+	}
+	if (hit_elem != NULL)
+	{
+		return (cast_ray(head, hit_elem, mult3(r.direction, distance)));
+//		return (rgb_to_int(((t_sphere *)hit_elem->content)->colour));
+	}
+	return (0);
 }
 
 int	main(int argc, char **argv)
@@ -48,29 +114,21 @@ int	main(int argc, char **argv)
 	t_data	img;
 	t_list 	**head;
 
-	if (argc == 2)
-    {
-		head = parser(argv[1]);
-		if (head == NULL)
-		{
-			ft_printf(2, "Error\nFile scene corrupted\n");
-			return (1);
-		}
-		else if (check_list_values(*head))
-		{
-			ft_printf(2, "Error\nInvalid value in scene\n");
-			ft_lstclear(head, free);
-			free(head);
-			return (1);
-		}
-    }
-	else
-    {
-        ft_printf(2, "Usage: ./miniRT (scene)\n");
-        return (1);
-    }
+
+	if (argc != 2)
+		return (ft_printf(2, "Usage: ./miniRT (scene)\n"));
+	head = parser(argv[1]);
+	if (head == NULL)
+		return (ft_printf(2, "Error\nFile scene corrupted\n"));
+	else if (check_list_values(*head))
+	{
+		ft_lstclear(head, free);
+		free(head);
+		return (ft_printf(2, "Error\nInvalid value in scene\n"));
+	}
+
     const double aspect_ratio = 16.0 / 9.0;
-    const int img_width = 480;
+    const int img_width = 720;
     const int img_height = (int)(img_width / aspect_ratio);
     double viewport_height = 2.0;
     double viewport_width = aspect_ratio * viewport_height;
@@ -101,7 +159,7 @@ int	main(int argc, char **argv)
             ray.direction = min_vec(
                     plus_vec(plus_vec(lower_left_corner, mult3(horizontal, w)),
                              mult3(vertical, h)), origin);
-            img.addr[px++] = ray_color(ray);
+            img.addr[px++] = ray_color(ray, head);
         }
     }
     mlx_hook(img.mlx_win, 17, 1L, ft_exit, &img);
